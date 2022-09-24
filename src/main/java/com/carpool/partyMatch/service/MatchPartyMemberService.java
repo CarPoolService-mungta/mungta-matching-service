e0456b33932588c974c6cd3a9915b850e4fa3ac8
LeeSeungJu <exceedlimit94@gmail.com>
2022-09-25 03:43:15 +0900

    API 조합 패턴 UI로 위임

--- src/main/java/com/carpool/partyMatch/service/MatchPartyMemberService.java
+++ src/main/java/com/carpool/partyMatch/service/MatchPartyMemberService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.carpool.partyMatch.client.dto.UserResponse;
 import com.carpool.partyMatch.controller.dto.response.MatchPartyMemberWithMatchStatusResponse;
 import com.carpool.partyMatch.controller.dto.response.MatchStatusAndMemberListResponse;
+import com.carpool.partyMatch.domain.MatchInfo;
 import com.carpool.partyMatch.domain.MatchStatus;
 
 public interface MatchPartyMemberService {
@@ -14,5 +15,7 @@ public interface MatchPartyMemberService {
   MatchStatusAndMemberListResponse findPartyMembersListSummaryAndMatchStatus(Long partyInfoId, String userId);
 
   List<UserResponse> findPartyMembersListSummary(Long partyInfoId);
+
+  List<MatchInfo> findWaitAndAcceptMembersByPartyInfoId(Long partyInfoId);
 }
 
